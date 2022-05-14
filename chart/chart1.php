2caa26dc8df1a27b818c3b7eef5cfac46e5b922f
momoclouq <phamhoangminh050899@gmail.com>
2022-05-14 23:17:42 +0700

    Fix wrong file chart 1

--- chart/chart1.php
+++ chart/chart1.php
@@ -1,64 +1,82 @@
 <?php
 
 #value from the ui
-$date2;
-$station2;
-$pollutant2;
+$year;
+$hour;
+$station;
 
 #arrays to store immediate value
-$levels = array_fill(0, 24, 0);
-
-// echo "date: " . $date2 . ", station: " . $station2 . ", pollutant: " . $pollutant2 . "<br>";
+$sums = array_fill(0, 12, 0);
+$counts = array_fill(0, 12, 0);
 
 #read the corresponding file
-$fileName = "../test/data-" . $station2 . ".xml";
+$fileName = "../test/data-" . $station . ".xml";
 if(!file_exists($fileName)) exit; 
 $xml = simplexml_load_file($fileName);
 
 foreach($xml->children() as $record) {
     $recordTime = new DateTime();
     $recordTime->setTimestamp(intval($record['ts']));
-
-    #get the date string
-    $formattedTime = $recordTime->format("Y-m-d");
     
-    if($formattedTime == $date2) {
-      $levels[intval($recordTime->format('H'))] = $record[$pollutant2];
+    $recordMonth = intval($recordTime->format('m'));
+    $recordHour = intval($recordTime->format('H'));
+    $recordYear = intval($recordTime->format('Y'));
+
+    if($recordYear == intval($year) && $recordHour == intval($hour)){
+        $noValue = floatval($record['no']);
+
+        $sums[$recordMonth - 1] += floatval($noValue);
+        $counts[$recordMonth - 1] += 1;
     }
 }
 
 #process result
 $processedResult = "";
 
-for($i = 0; $i < 24; $i++){
-    $processedResult .= "['" . ($i) . "' , " . $levels[$i] . "],";
+#set the maximum NO time to get the correct limit
+$maxNO = 0;
+
+for($i = 0; $i < 12; $i++){
+    if ($counts[$i] == 0) $average = 0;
+    else $average = $sums[$i] / $counts[$i];
+
+    if ($maxNO < $average) $maxNO = $average;
+    $processedResult .= "[" . ($i + 1) . " , " . $average . "],";
 }
 
-#format pollutant value
-$pollutant2 = strtoupper($pollutant2);
+#turn maxNO to int for google chart API limit
+$maxNO = ceil($maxNO);
 
 $output = <<<js
 <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Hour', '{$pollutant2}'],
-          {$processedResult}
-        ]);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Month', 'NO level'],
+    {$processedResult}
+  ]);
 
-        var options = {
-          title: '24 hour statistics on {$pollutant2} in {$date2}',
-          curveType: 'function',
-          subtitle: 'Concentration (µg/m³)'
-        };
+  var options = {
+    title: 'Average NO at time {$hour} hour, year: {$year} from {$xml['name']} station',
+    hAxis: {
+        title: 'Month', 
+        minValue: 1, 
+        maxValue: 12, 
+        gridlines: {
+            count: 6
+        }
+    },
+    vAxis: {title: 'NO', minValue: 0, maxValue: {$maxNO}},
+    legend: 'none'
+  };
 
-        var chart = new google.visualization.LineChart(document.getElementById('chart'));
+  var chart = new google.visualization.ScatterChart(document.getElementById('chart'));
 
-        chart.draw(data, options);
-      }
-    </script>
+  chart.draw(data, options);
+}
+</script>
 js;
 
 echo $output;
